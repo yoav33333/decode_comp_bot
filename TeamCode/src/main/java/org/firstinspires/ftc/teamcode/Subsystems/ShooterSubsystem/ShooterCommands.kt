c6d8d74de24cbe0e7ee3399b267eaf3aa4dbfebb
guyka2212 <2212kadosh@gmail.com>
2025-11-25 15:12:42 +0200

    added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem/ShooterCommands.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem/ShooterCommands.kt
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem
 
 import dev.nextftc.core.commands.utility.InstantCommand
 import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubsystem.IntakeHardware.setPower
-//import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem.ShooterVars.
+import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem.ShooterVars.targetVelocity
 
 object ShooterCommands {
-    val shoot = InstantCommand { setPower(p) }
+    val shoot = InstantCommand { setPower(targetVelocity) }
 
     val stopshoot = InstantCommand { setPower(0.0) }
 }
