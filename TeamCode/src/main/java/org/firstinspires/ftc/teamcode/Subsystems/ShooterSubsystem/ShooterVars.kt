ee5e1af1748249206367b917fef19547d064022c
guyka2212 <2212kadosh@gmail.com>
2025-11-25 10:21:15 +0200

    added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem/ShooterVars.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem/ShooterVars.kt
@@ -4,7 +4,7 @@ import com.bylazar.configurables.annotations.Configurable
 
 @Configurable
 object ShooterVars {
-    @JvmField var p = 0.0
+    @JvmField var p = 0.1
     @JvmField var i = 0.0
     @JvmField var d = 0.0
     @JvmField var targetVelocity = 0.0
